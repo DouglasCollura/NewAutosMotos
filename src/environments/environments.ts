95c18e7b3665de90b40abe32acc910a95aab8de4
DouglasCollura <91167407+DouglasCollura@users.noreply.github.com>
2022-12-01 20:15:05 -0400

    Create environments.ts

---
+++ src/environments/environments.ts
@@ -0,0 +1,10 @@
+
+export const environment:any = {
+  production: false,
+  serverUrl: 'https://www.backproduction.autosmotos.es/api/',
+  server: 'https://www.backproduction.autosmotos.es/',
+  apiKeyGeoCode:'AIzaSyBNxc68bA_M6w8Y1rSrAW35MnbDdc3tads'
+  //serverUrl: 'http://127.0.0.1:8000/api/',
+  //server: 'http://127.0.0.1:8000/'
+
+};
